41312b0868ca7d58d91a04f6d5a3c78f0c361c92
SrinivasNagaram <creator.srinivas@gmail.com>
2024-10-02 17:51:24 +0530

    Updated the profile page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,55 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-5xl font-extrabold">Srinivas Nagaram</h1>
+      <header className="text-center">
+        <h1 className="text-5xl font-extrabold pb-3">Srinivas Nagaram</h1>
+        <h2 className="pb-3 text-xl font-thin">Sr. Software Engineer</h2>
+      </header>
+      <main className="flex flex-col gap-8 row-start-2 sm:items-start text-center">
+        <ul>
+          <li className="pb-1">
+            <a
+              className="text-sm underline text-indigo-600"
+              href="https://www.linkedin.com/in/srinivasnagaram/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn/SrinivasNagaram
+            </a>
+          </li>
+          <li className="pb-1">
+            <a
+              className="text-sm underline text-indigo-600"
+              href="https://github.com/SrinivasNagaram"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub/SrinivasNagaram
+            </a>
+          </li>
+          <li className="pb-1">
+            <a
+              className="text-sm underline text-indigo-600"
+              href="https://x.com/SrinivasNagaram"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              X/SrinivasNagaram
+            </a>
+          </li>
+          <li className="pb-1">
+            <a
+              className="text-sm underline text-indigo-600"
+              href="https://www.instagram.com/srinivas.nagaram/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram/SrinivasNagaram
+            </a>
+          </li>
+        </ul>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex flex-wrap items-center justify-left">
         <p className="font-thin text-sm">
           © 2024 SrinivasNagaram. All rights reserved.
         </p>
