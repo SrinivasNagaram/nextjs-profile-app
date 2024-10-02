bbc9ef172a616d25cced665b2b6e727f3c5a604c
SrinivasNagaram <creator.srinivas@gmail.com>
2024-10-02 20:00:31 +0530

    Added background color to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="text-center">
         <h1 className="text-5xl font-extrabold pb-3">Srinivas Nagaram</h1>
         <h2 className="pb-3 text-xl font-thin">Sr. Software Engineer</h2>
